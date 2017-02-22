4ea344d7e5a6f8e213d0706a80f88d9e7ce9013c
Chris Gårdenberg <itssimple@users.noreply.github.com>
2017-02-22 16:26:38 +0100

    Changed blacklist to be lowercase, and changed the checks too

--- jsTimeLog.js
+++ jsTimeLog.js
@@ -1,19 +1,20 @@
 var jsTimeLog = window.jsTimeLog || {};
 jsTimeLog.MaxLevels = 5;
-jsTimeLog.Debug = false;
+jsTimeLog.Debug = true;
 jsTimeLog.CurrentLevel = 0;
 jsTimeLog.CurrentNS = 'window';
 jsTimeLog.BlackList = [
-    'jsTimeLog', 'parent', 'top', 'self', 'window', 'frames',
-    'document', 'external', '$', 'jQuery', 'theForm', 'Sys',
-    '__theFormPostCollection', '_events', 'ol_frame', 'Telerik',
-    'capExtent', 'ol_texts', 'ol_caps', 'olInfo', 'ol_text',
-    '$telerik', '$common', 'PRUM_EPISODES', 'console', 'chrome',
-    'memory', 'postParse', 'o3_frame', '_kgdata', 'window.$addHandler',
+    'jstimelog', 'parent', 'top', 'self', 'window', 'frames',
+    'document', 'external', '$', 'jquery', 'theform', 'sys',
+    '__theformpostcollection', '_events', 'ol_frame', 'telerik',
+    'capextent', 'ol_texts', 'ol_caps', 'olinfo', 'ol_text',
+    '$telerik', '$common', 'prum_episodes', 'console', 'chrome',
+    'memory', 'postparse', 'o3_frame', '_kgdata', 'window.$addhandler',
     'window.onload', 'window.$', '$addHandler', 'window.jQuery',
-    'window.$get', 'window.$create', 'window.$find', 'window.$addHandlers',
-    '$get', '$create', '$addHandler', '$addHandlers', 'window.theForm',
-    'TelerikCommonScripts', 'commonScripts', '__doPostBack', 'window.__doPostBack'
+    'window.$get', 'window.$create', 'window.$find', 'window.$addhandlers',
+    '$get', '$create', '$addhandler', '$addhandlers', 'window.theform',
+    'telerikcommonscripts', 'commonscripts', '__dopostback', 'window.__dopostback',
+    'tinymce'
 ];
 jsTimeLog.Init = function (obj, ns) {
     if (obj === undefined) { return; }
@@ -22,7 +23,7 @@ jsTimeLog.Init = function (obj, ns) {
     var functions = [];
 
     jsTimeLog.CurrentLevel++;
-    if (jsTimeLog.Debug) console.log("(" + jsTimeLog.CurrentLevel + ") Rewriting " + jsTimeLog.CurrentNS);
+    if (jsTimeLog.Debug) console.log("(" + jsTimeLog.CurrentLevel + "/" + jsTimeLog.MaxLevels + ") Rewriting " + jsTimeLog.CurrentNS);
 
     if (jsTimeLog.CurrentLevel >= jsTimeLog.MaxLevel) {
         if(jsTimeLog.Debug) console.log('Reached the maximum depth');
@@ -35,7 +36,7 @@ jsTimeLog.Init = function (obj, ns) {
                 functions.push(ownObj[i]);
             }
         } else if (typeof obj[ownObj[i]] === 'object' && obj[ownObj[i]] !== obj) {
-            if (jsTimeLog.BlackList.indexOf(ownObj[i]) !== -1)
+            if (jsTimeLog.BlackList.indexOf(ownObj[i].toLowerCase()) !== -1)
             {
                 if(jsTimeLog.Debug) console.log(ownObj[i] + ' is blacklisted');
                 continue;
@@ -65,10 +66,12 @@ jsTimeLog.RewriteFunction = function (inputFunction, objectName) {
     var lastBracket = oldFunction.lastIndexOf('}');
     var functionname = oldFunction.substring(9, firstArgument);
     var hasReturn = oldFunction.lastIndexOf('return') > -1;
-    var blacklisted = jsTimeLog.BlackList.indexOf(jsTimeLog.CurrentNS + '.' + objectName) !== -1;
+    var blacklisted = jsTimeLog.BlackList.indexOf((jsTimeLog.CurrentNS + '.' + objectName).toLowerCase()) !== -1;
 
     if (!blacklisted && eval(jsTimeLog.CurrentNS + "." + objectName) !== undefined) {
         var code = oldFunction.substring(firstBracket + 1, lastBracket);
+
+        
 
         code = jsTimeLog.CurrentNS + "." + objectName + " = function " + oldFunction.substring(9, firstBracket) + " {\n" +
 			"\tvar __jsT_Start = new Date().getTime();\n\n" +
